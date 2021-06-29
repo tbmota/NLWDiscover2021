a7bd2d5aa18d91cdde1d1d8435c1dba9ab37ae87
tbmota <thammyrys_@hotmail.com>
2021-06-28 23:57:56 -0300

    criando route e controllers

--- src/server.js
+++ src/server.js
@@ -10,6 +10,8 @@ server.use(express.static("public"))
 
 server.set('views', path.join(__dirname, 'views'))
 
+server.use(express.urlencoded({extended: true})) //pega o conteúdo do formulário, decodifica, e envia para o Controller
+
 server.use(route)
 
 server.listen(3000, () => console.log("RODANDO"))
