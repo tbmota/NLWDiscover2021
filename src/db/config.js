821ca283ef3d6c468169e9dd5c8c752398fb3a2a
tbmota <thammyrys_@hotmail.com>
2021-06-29 00:03:06 -0300

    iniciando o db

--- src/db/config.js
+++ src/db/config.js
@@ -1,8 +1,8 @@
 const sqlite3 = require("sqlite3");
 const {open} = require("sqlite"); //só preciso do open
 
-module.exports = () => {
+module.exports = () => 
     open({
-        filename: '/src/db/rocketq.sqlite'
-    })
-}
+        filename: '/src/db/rocketq.sqlite',
+        driver: sqlite3.Database, 
+    });
